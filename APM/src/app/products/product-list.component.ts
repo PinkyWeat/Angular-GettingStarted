f3b804fb344db23e24ebed00dcffe389428b807c
root <root@DLSMTQ3>
2023-05-05 15:20:28 -0300

    routing + errorHandling

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IProduct } from './products';
 import { ProductSerice } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = ' ';
+    sub!: Subscription;
 
     private _listFilter: string = '';
     get listFilter(): string {
@@ -39,9 +41,19 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart'
+        this.sub = this.productService.getProducts().subscribe({
+            // key-value fun -- key "next" - value: la fun
+            // next => que hacer cuando recibe la data
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 
     onRatingClicked(message: string){
